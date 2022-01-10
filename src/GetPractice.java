ad73578372939d7489412785936cbadf1fe2234d
shassans <shshah1352@gmail.com>
2022-01-10 12:28:47 -0500

    work 1 is done

--- src/GetPractice.java
+++ src/GetPractice.java
@@ -1,5 +1,12 @@
 public class GetPractice {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
+
+        //System.out.println("Hello world");
+
 }
+/*
+Set your username:
+git config --global user.name "FIRST_NAME LAST_NAME"
+
+Set your email address:
+git config --global user.email "MY_NAME@example.com"
+ */
